d6fb8c103fa94722b35f502356fff3069734a0ef
Aaditya Sinha <aadityasinha74.98@gmail.com>
2025-04-26 06:49:51 +0530

    Update wsgi.py

--- quiz_website/wsgi.py
+++ quiz_website/wsgi.py
@@ -12,5 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quiz_website.settings")
-
+import django
+django.setup()
+from django.core.management import call_command
+call_command('migrate')
 application = get_wsgi_application()
