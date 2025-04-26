7dab71c4fa121e944bb4c02867afa52039bf6c72
Aaditya Sinha <aadityasinha74.98@gmail.com>
2025-04-26 05:49:52 +0530

    Update settings.py

--- quiz_website/settings.py
+++ quiz_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-4@mg49-umm_!%2#p0f68hxdrks9v=#gdr6%&cz9m+e!aygl*@u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
